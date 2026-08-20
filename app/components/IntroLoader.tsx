53173e70122f21c6092e2a320181999077edc644
Jay Vyas <vyasjay85@gmail.com>
2026-08-20 12:18:15 +0530

    Revert "Color the loader blinds brand yellow"
    This reverts commit c46844008811b41f9f4f50c2e8c36536481aa4fa.

--- app/components/IntroLoader.tsx
+++ app/components/IntroLoader.tsx
@@ -66,7 +66,7 @@ export default function IntroLoader() {
           }}
           style={{
             flex: 1,
-            background: "#f7b538",
+            background: "var(--bg)",
             transformOrigin: "top",
           }}
         />
@@ -95,7 +95,7 @@ export default function IntroLoader() {
                 fontSize: 13,
                 fontWeight: 500,
                 letterSpacing: /[ऀ-ॿ　-鿿゠-ヿ]/.test(WORDS[index]) ? "0" : "0.18em",
-                color: "#780116",
+                color: "var(--accent-hover)",
                 userSelect: "none",
                 fontFamily: "inherit",
               }}
