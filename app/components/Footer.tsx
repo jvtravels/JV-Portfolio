fb8bfe32a24e27caad1749dcdddcf6eb974fbe09
Jay Vyas <vyasjay85@gmail.com>
2026-08-21 20:30:42 +0530

    Scale down footer crowd figures slightly
    scale=0.8 keeps faces legible while reducing how large each peep reads.

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -116,7 +116,7 @@ export default function Footer() {
           overflow: "hidden",
         }}
       >
-        <CrowdCanvas src="/images/peeps/all-peeps.png" rows={15} cols={7} />
+        <CrowdCanvas src="/images/peeps/all-peeps.png" rows={15} cols={7} scale={0.8} />
       </div>
     </footer>
   );
