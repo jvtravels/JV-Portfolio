76a37ad3e85b10eb3b3e6f97e3b5fa6aada69e02
Jay Vyas <vyasjay85@gmail.com>
2026-08-26 14:17:47 +0530

    Point robots.txt sitemap at canonical www host to avoid redirect

--- app/robots.ts
+++ app/robots.ts
@@ -1,9 +1,8 @@
 import type { MetadataRoute } from "next";
-import { SITE_URL } from "@/app/lib/site";
 
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: { userAgent: "*", allow: "/" },
-    sitemap: `${SITE_URL}/sitemap.xml`,
+    sitemap: "https://www.jayvyas.xyz/sitemap.xml",
   };
 }
